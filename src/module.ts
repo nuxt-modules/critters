c60f77358d1e559513ed2346b845d1aad8bd6c85
Daniel Roe <daniel@roe.dev>
2024-01-15 09:31:31 +0000

    fix: transform HTML files once `publicDir` is populated
    resolves https://github.com/nuxt-modules/critters/issues/311

--- src/module.ts
+++ src/module.ts
@@ -1,5 +1,7 @@
-import { defineNuxtModule, isNuxt2 } from '@nuxt/kit'
+import { readFile, writeFile } from 'node:fs/promises'
+import { defineNuxtModule, isNuxt2, useLogger } from '@nuxt/kit'
 import { resolve } from 'pathe'
+import { withoutLeadingSlash } from 'ufo'
 import Critters from 'critters'
 import type { Options } from 'critters'
 
@@ -22,22 +24,37 @@ export default defineNuxtModule<ModuleOptions>({
     // Only enable for production
     if (nuxt.options.dev) return
 
+    const logger = useLogger('critters')
+
     // Enable css extraction
     // @ts-expect-error TODO: use @nuxt/bridge-schema
     nuxt.options.build.extractCSS = true
 
     // Nitro handler (for prerendering only)
 
+    const generatedFiles = new Set<string>()
     nuxt.hook('nitro:init', nitro => {
+      nitro.hooks.hook('prerender:generate', route => {
+        if (!route.fileName?.endsWith('.html') || route.error) return
+        generatedFiles.add(withoutLeadingSlash(route.fileName))
+      })
+    })
+    nuxt.hook('nitro:build:public-assets', async nitro => {
       const critters = new Critters({
         path: nitro.options.output.publicDir,
         publicPath: nitro.options.baseURL,
         ...options.config,
       })
-      nitro.hooks.hook('prerender:generate', async route => {
-        if (!route.fileName?.endsWith('.html') || route.error || !route.contents) return
-        route.contents = await critters.process(route.contents)
-      })
+      for (const file of generatedFiles) {
+        try {
+          const path = resolve(nitro.options.output.publicDir, file)
+          const contents = await readFile(path, 'utf-8')
+          const processed = await critters.process(contents)
+          await writeFile(path, processed)
+        } catch {
+          logger.log(`Could not inline CSS in \`${file}\`.`)
+        }
+      }
     })
 
     /* c8 ignore start */
@@ -56,7 +73,7 @@ export default defineNuxtModule<ModuleOptions>({
         try {
           result.html = await critters.process(result.html)
         } catch (e) {
-          console.log(e)
+          logger.log(e)
         }
       })
 
